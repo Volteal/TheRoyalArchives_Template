c4cd42aceab34fd256c1dfcd31c455942364f2c1
Volteal <wendy.a.knight@icloud.com>
2024-06-24 00:58:07 +0200

    fixed the site for mobile, tablet, and PC use.

--- src/public/js/year.js
+++ src/public/js/year.js
@@ -2,3 +2,4 @@ year = new Date().getFullYear();
 
 document.getElementsByClassName('year')[0].innerHTML = year;
 document.getElementsByClassName('year')[1].innerHTML = year;
+document.getElementsByClassName('year')[2].innerHTML = year;
